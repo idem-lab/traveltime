97eab10624f2834a0f90f56975a17a7da9de272f
geryan <geryan@users.noreply.github.com>
2024-05-08 16:46:06 +1000

    Somewhere you will find a place where men live without fear.

---
+++ tests/testthat/test-get_friction_surface.R
@@ -0,0 +1,27 @@
+test_that("Are SpatRasters", {
+  ext <- matrix(
+    data = c("111", "0", "112", 1),
+    nrow = 2,
+    ncol = 2,
+    dimnames = list(
+      c("x", "y"),
+      c("min", "max")
+    )
+  )
+
+  mfs <- get_friction_surface(
+    surface = "motor2020",
+    file_name = tempfile(fileext = ".tif"),
+    extent = ext
+  )
+
+  expect_s4_class(mfs, "SpatRaster")
+
+  wfs <- get_friction_surface(
+    surface = "walk2020",
+    file_name = tempfile(fileext = ".tif"),
+    extent = ext
+  )
+
+  expect_s4_class(wfs, "SpatRaster")
+})
