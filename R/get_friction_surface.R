71ba13b168052b891662ee413d9607010ac931c6
geryan <geryan@users.noreply.github.com>
2024-05-08 15:14:41 +1000

    Corri uomo, vai

--- R/get_friction_surface.R
+++ R/get_friction_surface.R
@@ -1,8 +1,23 @@
+#' @title Get friction surface
+#' @details Wrapper function to download friction surfaces via `malariaAtlas::getRaster``
+#'
+#' @param surface
+#' @param file_name
+#' @param overwrite
+#' @param extent
+#'
+#' @return
+#' @export
+#'
+#' @examples
 get_friction_surface <- function(
   surface = c("motor2020", "walk2020"),
   file_name = "friction_surface.tif",
-  overwrite = FALSE
+  overwrite = FALSE,
+  extent = NULL
 ){
+
+  #matrix(c("0", "0", "1", "1"), nrow = 2, ncol = 2, dimnames = list(c("x", "y"), c("min", "max")))
   # function goes and downloads a friction surface
   surface <- match.arg(surface)
 
@@ -16,4 +31,18 @@ get_friction_surface <- function(
 
     return(terra::rast(file_name))
   }
+
+  if(surface == "motor2020"){
+    surface_name <- "Explorer__2020_motorized_friction_surface"
+  } else if (surface == "walk2020"){
+    surface_name <- "Explorer__2020_walking_only_friction_surface"
+  }
+
+  malariaAtlas::getRaster(
+    dataset_id = surface_name,
+    extent = extent
+  ) |>
+    terra::rast() |>
+    sdmtools::writereadrast(filename = file_name)
+
 }
