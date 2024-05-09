db6bb1047204cdc02100f7898fe2ea4c77ee5e15
geryan <geryan@users.noreply.github.com>
2024-05-09 16:30:46 +1000

    And you′re panting like hare, like deer like a rabbit

--- R/calculate_travel_time.R
+++ R/calculate_travel_time.R
@@ -1,11 +1,22 @@
+#' @title Calculate travel time
+#' @description
+#' Implements Weiss et al method to calculate travel time given a
+#'
+#'
+#' @param friction_surface
+#' @param points
+#' @param travel_time_filename
+#' @param overwrite_raster
+#'
+#' @return
+#' @export
+#'
+#' @examples
 calculate_travel_time <- function(
     friction_surface,
     points,
-    t_filename = "outputs/areaT.rds",
-    tgc_filename = "outputs/areaTGC.rds",
     travel_time_filename = "outputs/travel_time.tif",
-    overwrite_raster = FALSE,
-    overwrite_t = FALSE
+    overwrite_raster = FALSE
 ){
 
   if(file.exists(travel_time_filename) & !overwrite_raster){
@@ -24,25 +35,22 @@ calculate_travel_time <- function(
 
   friction <- raster::raster(friction_surface)
 
-  if(!file.exists(t_filename) | (file.exists(t_filename) & overwrite_t)){
-    tsn <- transition(friction, function(x) 1/mean(x), 8) # RAM intensive, can be very slow for large areas
-    saveRDS(tsn, t_filename)
-  } else {
-    tsn <- readRDS(t_filename)
-  }
-
-  tgc <- geoCorrection(tsn)
-  saveRDS(tgc, tgc_filename)
+  tsn <- gdistance::transition(friction, function(x) 1/mean(x), 8) # RAM intensive, can be very slow for large areas
+  tgc <- gdistance::geoCorrection(tsn)
 
   xy.data.frame <- data.frame()
   xy.data.frame[1:npoints,1] <- points[,1]
   xy.data.frame[1:npoints,2] <- points[,2]
   xy.matrix <- as.matrix(xy.data.frame)
 
-  temp.raster <- accCost(tgc, xy.matrix)
+  travel_time <- gdistance::accCost(tgc, xy.matrix)
 
-  writeRaster(temp.raster, travel_time_filename, overwrite = overwrite_raster)
+  raster::writeRaster(
+    travel_time,
+    travel_time_filename,
+    overwrite = overwrite_raster
+  )
 
-  rast(travel_time_filename)
+  terra::rast(travel_time_filename)
 
 }
