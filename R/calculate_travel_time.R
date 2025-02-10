535ec2f3bdcc578beb4db9ed4f7897536dd07200
geryan <geryan@users.noreply.github.com>
2025-02-10 16:43:04 +1100

    tidy and link documentation for `calculate_travel_time`

--- R/calculate_travel_time.R
+++ R/calculate_travel_time.R
@@ -3,8 +3,8 @@
 #'   surface.
 #'
 #'
-#' @param friction_surface A `SpatRaster` friction surface layer. See
-#'   `?get_friction_surface`
+#' @param friction_surface A `SpatRaster` friction surface layer with in
+#'   resistance units. See Details.
 #' @param points A two-column `matrix`, `data.frame`, (including `tibble` types)
 #'   with longitude (x) in the first column and latitude (y) in the second, or a
 #'   `SpatVector`, in the same coordinate, reference system as
@@ -19,20 +19,32 @@
 #'   Over large areas this function can require significant RAM and will be
 #'   slow.
 #'
-#'   Citations: D. J. Weiss, A. Nelson, C. A. Vargas-Ruiz, K. Gligoric, S.,
-#'   Bavadekar, E. Gabrilovich, A. Bertozzi-Villa, J. Rozier, H. S. Gibson, T.,
-#'   Shekel, C. Kamath, A. Lieber, K. Schulman, Y. Shao, V. Qarkaxhija, A. K.
-#'   Nandi, S. H. Keddie, S. Rumisha, P. Amratia, R. Arambepola, E. G.
-#'   Chestnutt, J. J. Millar, T. L. Symons, E. Cameron, K. E. Battle, S. Bhatt,
-#'   and P. W. Gething. Global maps of travel time to healthcare facilities.
-#'   (2020) Nature Medicine. https://doi.org/10.1038/s41591-020-1059-1
+#'   Pre-prepared walking or motorised friction surfaces can be obtained
+#'   with`?get_friction_surface`. User can also provide their own friction
+#'   surface. This surface but must be in resistance units (1/conductance), e.g.
+#'   minutes/meter. See Van Etten 2017.
+#'
+#'   Citations:
+#'
+#'   D. J. Weiss, A. Nelson, C. A. Vargas-Ruiz, K. Gligoric, S., Bavadekar, E.
+#'   Gabrilovich, A. Bertozzi-Villa, J. Rozier, H. S. Gibson, T., Shekel, C.
+#'   Kamath, A. Lieber, K. Schulman, Y. Shao, V. Qarkaxhija, A. K. Nandi, S. H.
+#'   Keddie, S. Rumisha, P. Amratia, R. Arambepola, E. G. Chestnutt, J. J.
+#'   Millar, T. L. Symons, E. Cameron, K. E. Battle, S. Bhatt, and P. W.
+#'   Gething. Global maps of travel time to healthcare facilities. (2020) Nature
+#'   Medicine. <https://doi.org/10.1038/s41591-020-1059-1>
 #'
 #'   D. J. Weiss, A. Nelson, H.S. Gibson, W. Temperley, S. Peedell, A. Lieber,
 #'   M. Hancher, E. Poyart, S. Belchior, N. Fullman, B. Mappin, U. Dalrymple, J.
 #'   Rozier, T.C.D. Lucas, R.E. Howes, L.S. Tusting, S.Y. Kang, E. Cameron, D.
 #'   Bisanzio, K.E. Battle, S. Bhatt, and P.W. Gething. A global map of travel
 #'   time to cities to assess inequalities in accessibility in 2015. (2018).
-#'   Nature. doi:10.1038/nature25181.
+#'   Nature. <https://doi:10.1038/nature25181>
+#'
+#'   van Etten, J. (2017). R package gdistance: Distances and routes on
+#'   geographical grids. Journal of Statistical Software
+#'   <https://doi.org/10.18637/jss.v076.i13>
+#'
 #'
 #'
 #' @return `SpatRaster`
