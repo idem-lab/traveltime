560133f3a070eac37382b3898be3e416b0f8773b
geryan <geryan@users.noreply.github.com>
2025-03-05 17:33:07 +1100

    100% test coverage mfers

--- tests/testthat/test-ext_matrix.R
+++ tests/testthat/test-ext_matrix.R
@@ -18,3 +18,84 @@ test_that(
     )
   }
 )
+
+test_that(
+  desc = "ext_matrix fails if too long",
+  code = {
+
+    x <- c(1,2:10)
+
+    expect_error(
+      object = ext_matrix(x)
+    )
+  }
+)
+
+test_that(
+  desc = "ext_matrix fails if double too short",
+  code = {
+
+    x <- 1
+
+    expect_error(
+      object = ext_matrix(x)
+    )
+  }
+)
+
+test_that(
+  desc = "ext_matrix fails if integer incorrect length",
+  code = {
+
+    x <- c(1,2)
+
+    expect_error(
+      object = ext_matrix(x)
+    )
+  }
+)
+
+
+
+test_that(
+  desc = "ext_matrix fails if dim incorrect",
+  code = {
+
+    x <- matrix(1)
+
+    expect_error(
+      object = ext_matrix(x)
+    )
+  }
+)
+
+
+test_that(
+  desc = "ext_matrix fails if inappropriate class",
+  code = {
+
+    x <- "junk"
+
+    expect_error(
+      object = ext_matrix(x)
+    )
+  }
+)
+
+test_that(
+  desc = "ext_matrix parses double",
+  code = {
+
+    x <- c(111, 112, 0, 1)
+
+    y <- ext_matrix(extent = x)
+
+    expect_true(
+      object = inherits(
+        x = y,
+        what = "matrix"
+      )
+    )
+  }
+)
+
